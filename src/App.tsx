00411b641a336331f8990a220e3317dbe8792e69
CAVASOL <awyeon@gmail.com>
2023-04-28 23:27:43 +0800

    ah

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-import { useHelpers, useRemirrorContext } from "@remirror/react";
+import { useHelpers, useRemirrorContext, useKeymap } from "@remirror/react";
 import { SocialEditor } from "@remirror/react-editors/social";
 
 const ALL_USERS = [
@@ -8,9 +8,12 @@ const ALL_USERS = [
   { id: "pat", label: "Pat" },
   { id: "tom", label: "Tom" },
   { id: "jim", label: "Jim" },
+  { id: "sol", label: "Sol" },
 ];
 
-const TAGS = ["editor", "remirror", "opensource", "prosemirror"];
+// take tags somewhere and restore them to the editor
+
+const TAGS = ["editor", "remirror", "opensource", "prosemirror", "whatever"];
 
 const SAMPLE_DOC = {
   type: "doc",
@@ -68,3 +71,65 @@ const Basic: React.FC = () => {
 };
 
 export default Basic;
+
+// import { Remirror, ThemeProvider, Toolbar, useRemirror } from "@remirror/react";
+// import { AllStyledComponent } from "@remirror/styles/emotion";
+// import {
+//   BoldExtension,
+//   HeadingExtension,
+//   ItalicExtension,
+//   UnderlineExtension,
+// } from "remirror/extensions";
+
+// import {
+//   BasicFormattingButtonGroup,
+//   DataTransferButtonGroup,
+//   HeadingLevelButtonGroup,
+//   HistoryButtonGroup,
+//   VerticalDivider,
+// } from "@remirror/react";
+
+// const extensions = () => [
+//   new HeadingExtension(),
+//   new BoldExtension(),
+//   new ItalicExtension(),
+//   new UnderlineExtension(),
+// ];
+
+// function EditorToolbar() {
+//   return (
+//     <Toolbar>
+//       <HistoryButtonGroup />
+//       <VerticalDivider />
+//       <DataTransferButtonGroup />
+//       <VerticalDivider />
+//       <HeadingLevelButtonGroup />
+//       <VerticalDivider />
+//       <BasicFormattingButtonGroup />
+//       <VerticalDivider />
+//     </Toolbar>
+//   );
+// }
+
+// export default function Editor() {
+//   const { manager, state } = useRemirror({
+//     extensions,
+//     content: "<p><u>Hello</u> there <b>friend</b> and <em>partner</em>.</p>",
+//     selection: "end",
+//     stringHandler: "html",
+//   });
+//   return (
+//     <AllStyledComponent>
+//       <ThemeProvider>
+//         <Remirror
+//           manager={manager}
+//           initialContent={state}
+//           autoFocus
+//           autoRender="end"
+//         >
+//           <EditorToolbar />
+//         </Remirror>
+//       </ThemeProvider>
+//     </AllStyledComponent>
+//   );
+// }
